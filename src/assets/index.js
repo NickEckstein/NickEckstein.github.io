00fa0b7fda35f56176618be108a8067e2ec7438d
Nick Eckstein <ecksnl@farmingdale.edu>
2025-01-15 06:58:10 -0500

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import pytorch from "./tech/pytorch.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import illustrator from "./tech/illustrator.png";
+import python from "./tech/python.png";
+import photoshop from "./tech/photoshop.png";
+import threejs from "./tech/threejs.svg";
+
+import color from "./company/color.png";
+
+import tshirtdotco from "./tshirtdotco.png";
+import carapi from "./carapi.png";
+import llm from "./llm.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  pytorch,
+  nodejs,
+  reactjs,
+  illustrator,
+  python,
+  photoshop,
+  threejs,
+  color,
+  tshirtdotco,
+  carapi,
+  llm,
+};
