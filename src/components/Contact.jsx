9bf1d74bca721bfb0118a4ff456cc651fa117ee7
Nick Eckstein <124191262+NickEckstein@users.noreply.github.com>
2025-01-15 08:32:48 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_czhhx4d',
+        'template_2su2d4k',
         {
           from_name: form.name,
           to_name: "Nick Eckstein",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "nickeckstein107@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '8ibVxmAiewn0WOmqU'
       )
       .then(
         () => {
